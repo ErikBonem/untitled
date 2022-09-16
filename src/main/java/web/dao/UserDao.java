2e0fdd50a75f35f5466b85ed9a1a713242cdc087
ErikBonem <yunusoverik@mail.ru>
2022-09-16 19:40:31 +0300

    123

--- src/main/java/web/dao/UserDao.java
+++ src/main/java/web/dao/UserDao.java
@@ -2,7 +2,6 @@ package web.dao;
 
 import org.springframework.stereotype.Repository;
 import web.models.User;
-
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
@@ -16,15 +15,12 @@ public class UserDao {
     public User show(int id){
         return em.find(User.class,id);
     }
-
     public void save(User user){
         em.persist(user);
     }
-
     public void update (User user){
         em.merge(user);
     }
-
     public void delete(int id){
         em.remove(show(id));
     }
